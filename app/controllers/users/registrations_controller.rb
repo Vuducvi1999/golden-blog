cef15a16ab723fdb08c8ff870b38e1937afaf1e6
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-06-16 10:52:12 +0700

    first strike

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :check_username_exist, only: %i[update]
+  before_action :check_update_field, only: %i[update]
 
-  def check_username_exist 
+  def check_update_field
     find_record = User.where(username: params[:user][:username])
+
     if find_record.present?
       flash[:alert] = 'username already exist'
       
-      # redirect_to edit_user_registration_path
-    elsif params[:user][:current_password].empty?
-      flash.alert = "Current password can\\'t be blank"
+    elsif params[:user][:current_password].blank?
+      flash[:alert] = "Current password can\\'t be blank"
       
-      # redirect_to edit_user_registration_path
     end
-
   end
+  
 end
