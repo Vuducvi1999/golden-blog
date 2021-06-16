a96f272f60dc5bbc0382a48fd9b54ff344ec57d5
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-06-16 15:39:17 +0700

    pendding fix content post trix text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,6 @@ global.toastr = require("toastr")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { confirmations: 'users/confirmations',
   omniauth_callbacks: 'users/omniauth_callbacks',
   registrations: 'users/registrations' }
+  
+  resources :categories
+  
+  resources :posts
+
 
   
 end
