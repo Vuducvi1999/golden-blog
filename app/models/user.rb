f0b436e4ba77b2ca2c94d12b243348ce2c04d871
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-06-17 16:33:33 +0700

    add thumbnail and short description to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable,
          :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
-  has_many :posts 
+  has_many :posts, dependent: :destroy
 
   def self.from_omniauth(access_token)
     data = access_token.info
