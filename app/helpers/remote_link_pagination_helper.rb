42d17ae8ca21d43019a01259f76ef2109635ebae
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-06-26 21:25:12 +0700

    fix bug will_paginate and change serve public file

--- app/helpers/remote_link_pagination_helper.rb
+++ app/helpers/remote_link_pagination_helper.rb
@@ -1,4 +1,6 @@
 module RemoteLinkPaginationHelper
+  require 'will_paginate/view_helpers/link_renderer'
+  require 'will_paginate/view_helpers/action_view'  
   class LinkRenderer < WillPaginate::ActionView::LinkRenderer
     def link(text, target, attributes = {})
       attributes['data-remote'] = true
