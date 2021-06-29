40185fb4830e1bbd804bd63c6fc3767fc2480bfc
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-06-29 17:01:54 +0700

    fix bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,6 @@ class Users::SessionsController < Devise::SessionsController
   before_action :destroy_cart, only: :destroy
 
   def destroy_cart
-    current_user.update access_token:''
+    # current_user.update access_token:''
   end
 end
