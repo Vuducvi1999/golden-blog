522edaad7c49ced29882c2d9a56f9aa641055b0a
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-07-03 21:10:10 +0700

    change default value post

--- db/migrate/20210703140743_change_post_facebook_default_to_false.rb
+++ db/migrate/20210703140743_change_post_facebook_default_to_false.rb
@@ -1,5 +1,5 @@
 class ChangePostFacebookDefaultToFalse < ActiveRecord::Migration[6.1]
   def change
-    change_column :posts, :post_facebook?, :boolean, default:nil
+    change_column :posts, :post_facebook?, :boolean, default: false
   end
 end
