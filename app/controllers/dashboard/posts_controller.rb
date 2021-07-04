5b8029c804924e42313ca375a25224800cfb984c
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-07-04 14:42:38 +0700

    .

--- app/controllers/dashboard/posts_controller.rb
+++ app/controllers/dashboard/posts_controller.rb
@@ -43,6 +43,15 @@ class Dashboard::PostsController < ApplicationController
           message: @post.text_content,
           link: post_url(@post)
         })
+
+        puts post_url(@post)
+        puts post_url(@post)
+        puts post_url(@post)
+        puts post_url(@post)
+        puts post_url(@post)
+        puts post_url(@post)
+        puts post_url(@post)
+        
         @post.update post_facebook_id: object['id']
       end
       redirect_to @post, notice: "Post was successfully created." 
