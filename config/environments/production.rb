6c48ee557c9355e60dd9d7b694b892786a8f5a06
Vuducvi1999 <57878618+Vuducvi1999@users.noreply.github.com>
2021-07-20 20:11:15 +0700

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.configure doz
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.default_url_options = { :host => 'vuducvi-golden-blog.herokuapp.com' }
