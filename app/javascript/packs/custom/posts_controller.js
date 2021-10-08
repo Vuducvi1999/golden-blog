80e2aaca55d4cafd19c401756ff3a433c579417d
Vuducvi1999 <vuducvi20@gmail.com>
2021-10-08 10:15:56 +0700

    fixbug share facebook

--- app/javascript/packs/custom/posts_controller.js
+++ app/javascript/packs/custom/posts_controller.js
@@ -2,18 +2,18 @@
 document.addEventListener('turbolinks:load',()=>{
   const clickers = $('#change-status-actions a')
   
-  // // clickers.toArray().forEach((clicker)=>{
-  // //   clicker.on('click', function() {
-  // //     console.log($(this).nextAll('.spinner-border'))
-  // //     $(this).nextAll('.spinner-border').addClass('d-block').removeClass('d-none')
-  // //   })
-  // // })
-  // const clickers = $('#test-click-me')
-  
-  clickers.toArray().forEach((clicker)=>{
+  clickers.toArray().forEach((clicker) => {
     $(clicker).click(()=>{
       $(clicker).nextAll('.spinner-border').addClass('d-block').removeClass('d-none')
       clickers.toArray().forEach(item=> $(item).hide())
     })
+  })
+
+  $('#share-facebook-btn').click(() => {
+    FB.ui({
+      display: 'popup',
+      method: 'share',
+      href: "https://github.com/heartcombo/devise",
+    }, function(response){});
   })
 })
